a25f0dc46094dd9ea4c58fbacc987af73fe953fe
Milagros Lechares <80794759+mflec@users.noreply.github.com>
2021-09-09 13:31:11 -0300

    modifing post in home

--- api/src/controllers/home.js
+++ api/src/controllers/home.js
@@ -4,7 +4,7 @@ const { v4: uuidv4 } = require('uuid');
 const posthome = async (req, res) => {
     const { content } = req.body;
     const username = req.session.userId
-    
+
     try {
         const tweet = await Tweet.create({id: uuidv4(), content, username: req.session.userId });
         const user = await User.findOne({ where: { username: username } });
@@ -13,7 +13,7 @@ const posthome = async (req, res) => {
 
     } catch (error) {
         console.log(error)
-        res.status(500).send('Upss 😥')
+        return res.redirect('/home');
     }
 }
 
